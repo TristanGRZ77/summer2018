7ce7a544560e17e8f53ba496d6020e57b0e461ed
TristanGRZ77 <t.bouteville@gmail.com>
2018-08-14 14:03:39 +0200

    Affiche l'heure en temps réel dans une fenetre

--- 20180808/Heure.py
+++ 20180808/Heure.py
@@ -1,5 +1,19 @@
 from Tkinter import *
+import time
+import threading
+
+def temps():
+    global label
+    while True:
+        heure = time.strftime('%H:%M:%S', time.localtime())
+        label.config(text = heure)
+        time.sleep(0.1)
+
 fenetre = Tk()
-label = Label(fenetre, text = "Message")
+label = Label(fenetre, text = "")
 label.pack()
-fenetre.mainloop
+
+T = threading.Thread(target = temps)
+T.start()
+
+fenetre.mainloop()
