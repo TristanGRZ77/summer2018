bd4af7e1a329e2b21816daf32b02497426faa1ab
TristanGRZ77 <t.bouteville@gmail.com>
2018-08-24 21:01:38 +0200

    Version Papa

--- 20180824/image2.py
+++ 20180824/image2.py
@@ -1,12 +1,10 @@
 from Tkinter import *
 
-w = True
-
+# Definition des variables
 root = Tk()
 global x
 global y
-
-# Definition des variables
+global _job
 
 largeurEcran = 320
 hauteurEcran = 320
@@ -15,131 +13,112 @@ x = largeurEcran / 2
 img = PhotoImage(file = "/Users/tristanbouteville/Documents/Yacht.512.ppm")
 largeurImage= img.width()
 hauteurImage = img.height()
-
 xmax = largeurEcran - (largeurImage/2) - 1
 xmin = (largeurImage / 2) - 1
 ymax = hauteurEcran - (hauteurImage/2) - 1
 ymin = (hauteurImage / 2) + 1
-
 can = Canvas(root, width = largeurEcran, height = hauteurEcran, bg = "black")
+tempsDeplacement = 100
+_job = None
+pasDeplacement = 10
 
 # Fonction qui replace l'image au centre
-
 def home():
-    stop2()
-    action(largeurEcran/2,hauteurEcran/2)
+    global x
+    global y
+    deplacementStop()
+    x = largeurEcran/2
+    y = hauteurEcran/2
+    action(x,y)
 
 # Boutons directionnels et deplacements
-
-def boutonHaut():
+def deplacementHaut(_temps, _x, _y):
     global x
     global y
-    global w
+    global _job
     if (y < ymin):
         y = ymin
-    if w != True:
-        w = True
-        return root.mainloop()
+        deplacementStop()
     else:
-        y -= 1
-        deplacementHaut()
-    action(x,y)
+        y -= pasDeplacement
+        action(x,y)
+        _job = can.after(tempsDeplacement, deplacementHaut, tempsDeplacement, x, y)
 
-def boutonBas():
+def deplacementBas(_temps, _x, _y):
     global x
     global y
-    global w
+    global _job
     if (y > ymax):
         y = ymax
-    if w != True:
-        w = True
-        return root.mainloop()
+        deplacementStop()
     else:
-        y += 1
-        deplacementBas()
-    action(x,y)
+        y += pasDeplacement
+        action(x,y)
+        _job = can.after(tempsDeplacement, deplacementBas, tempsDeplacement, x, y)
 
-def boutonGauche():
+def deplacementGauche(_temps, _x, _y):
     global x
     global y
-    global w
+    global _job
     if (x < xmin):
         x = xmin
-    if w != True:
-        w = True
-        return root.mainloop()
+        deplacementStop()
     else:
-        x -= 1
-        deplacementGauche()
-    action(x,y)
+        x -= pasDeplacement
+        action(x,y)
+        _job = can.after(tempsDeplacement, deplacementGauche, tempsDeplacement, x, y)
 
-def boutonDroit():
+def deplacementDroit(_temps, _x, _y):
     global x
     global y
-    global w
+    global _job
     if (x > xmax):
         x = xmax
-    if w != True:
-        w = True
-        return root.mainloop()
+        deplacementStop()
     else:
-        x += 1
-        deplacementDroit()
-    action(x,y)
+        x += pasDeplacement
+        action(x,y)
+        _job = can.after(tempsDeplacement, deplacementDroit, tempsDeplacement, x, y)
 
-def deplacementHaut():
-    can.after(100, boutonHaut)
+def boutonHaut():
+    deplacementStop()
+    deplacementHaut(tempsDeplacement, x, y)
+    
+def boutonBas():
+    deplacementStop()
+    deplacementBas(tempsDeplacement, x, y)
 
-def deplacementBas():
-    can.after(100, boutonBas)
+def boutonGauche():
+    deplacementStop()
+    deplacementGauche(tempsDeplacement, x, y)
 
-def deplacementGauche():
-    can.after(100, boutonGauche)
-
-def deplacementDroit():
-    can.after(100, boutonDroit)
+def boutonDroit():
+    deplacementStop()
+    deplacementDroit(tempsDeplacement, x, y)
 
 # Fonction qui arrete le deplacement de l'image
-
-def stop():
-    global w
-    w = False
-
-# Fonction liee a la fonction home, qui arrete le deplacement de l'image et replace les coordonees du centre de l'image au centre de la fenetre, 
-# ce qui permet de pouvoir utiliser a nouveau les boutons directionnels depuis le centre de la fenetre et non la ou l'image s'est arretee
-
-def stop2():
-    global w
-    global x
-    global y
-    w = False
-    action(160, 160)
-    x = 160
-    y = 160
+def deplacementStop():
+    global _job
+    if _job is not None:
+        root.after_cancel(_job)
+        _job = None
 
 # Actualise les coordonees du centre de l'image
-
 def action(_x, _y):
     can.delete("all") 
     can.create_image(_x, _y, image = img)
 
 # Affiche la fenetre
-
 def affichage():
+    global _job
+    _job = None
     can.grid(column = 1, row = 0)
-
     Button(root, text = "Recentrer l'image", command = home ).grid(column = 1, row = 2)
-
     Button(root, text = "Gauche", command = boutonGauche ).grid(column = 0, row = 2)
-
     Button(root, text = "Droite", command = boutonDroit ).grid(column = 2, row = 2)
-
     Button(root, text = "Haut", command = boutonHaut ).grid(column = 1, row = 1)
-
     Button(root, text = "Bas", command = boutonBas ).grid(column = 1, row = 3)
-
-    Button(root, text = "Stop", command = stop).grid(column = 1, row = 4)
-
+    Button(root, text = "Stop", command = deplacementStop ).grid(column = 1, row = 4)
     home()
 
 affichage()
