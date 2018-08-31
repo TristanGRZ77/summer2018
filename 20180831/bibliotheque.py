bfc23986ffa838dc91a2aa7f7d13d071fe43403f
Gitea <gitea@fake.local>
2018-09-01 00:23:45 +0200

    Version Papa - Le bouton "mode" se nomme "Sim"ou "Sci" selon le mode sélectionné - Les écrans de résultats sont des écrans de résultats et non plus des zones de saisie.
    => je te laisse terminer pour le mode scientifique et gérer les erreurs

--- 20180831/bibliotheque.py
+++ 20180831/bibliotheque.py
@@ -8,68 +8,69 @@ from math import *
 
 root = Tk.Tk()
 t, y, n = 0, 0, 1
-
-# fonction qui permet l'affichage de facteurs a plusieurs chiffres
-
-def nombre(x):
-    global t, n   
-    t = t * 10 + x
-    u.set(str(t))
-
-#def lettre(x):
-#    global t, n
-#    u.set(str(t))
+effacer_resultat = 0
+resultat_calcul = 0
+# zone de stockage de la premiere operande
+operande0 = 0
+# zone de stockage de la seconde operande
+operande1 = 0
+# string temporaire de stockage des operandes
+_operande = ""
+# indicateur operande en cours
+switch_operande = 0
+signe = ""
 
 # fonction liee aux boutons chiffres qui saisissent les valeurs desirees
 
-def x(_nombre):
-    nombre(_nombre)
+# utliser les focntions de concatenation de string
+# lorsque un signe est saisi, convertir en entier puis stocker la variable
+# pareillement pour le second operande.
+# lorsaue la touche egal est appuyer, les operations arithmetiques sont effectuees
 
-# t1 correspond au deuxieme facteur de l'operation
-# fonction d'operation
+def f_x(_nombre):
+    global signe, _operande
+    _operande = _operande + str(_nombre)
+    if signe == "":
+        _label_calcul.set(_operande)
+        _label_resultat.set("")
+    else:
+        _label_calcul.set(label_calcul.cget("text") + str(_nombre))
 
 def operation(_signe):
-    global y, t1, t, n
-    y, t1, n = _signe, t, 1
-    t = 0
+    global operande0, _operande, signe
+    signe = _signe
+    operande0 = int(_operande)
+    _label_calcul.set(_operande + str(_signe))
+    _operande = ""
         
 def aegal():
-    global t, t1, n
-    z.set("0")
-    if y == '+':
-        z.set(str((t1 + t)))
-        t = t1 + t
-    elif y == '-':
-        z.set(str((t1 - t)))
-        t = t1 - t
-    elif y == '*':
-        z.set(str((t1 * t)))
-        t = t1 * t
-    elif y == '/':
-        if t == 0:
-            z.set(str("Erreur, impossible"))
-        else:
-            z.set(str((t1 / t)))
-            t = t1 / t
-    elif y == '%':
-        if t == 0:
-            z.set(str("Erreur, impossible"))
-        else:
-            z.set(str((t1 % t)))
-            t = t1 % t
+    global signe, operande0, operande1, _operande
+    operande1 = int(_operande)
+    if signe == '+':
+        resultat_operation = operande0 + operande1
+    elif signe == '-':
+        resultat_operation = operande0 - operande1
+    elif signe == '*':
+        resultat_operation = operande0 * operande1
+    elif signe == '/':
+        resultat_operation = operande0 / operande1
+    elif signe == '%':
+        resultat_operation = operande0 % operande1
 
-    t1, n = 0, 1
-    clear(u)
+    _label_resultat.set(str(resultat_operation))
+    signe = ""
+    _operande = ""
 
 def clear(_clear):
-    global t, t1, n, z, u, y
-    t, t1, n, y = 0, 0, 0, 0
-    _clear.set(str('0'))
+    global operande0, operande1, _label_resultat, _label_calcul
+    operande0, operande1 = 0, 0
+    _label_resultat.set("")
+    _label_calcul.set("")
 
 def changeMode():
-    lblmode = _mode.get().rstrip()
-    if lblmode == "Simple":
-        _mode.set("Scientifique")
+    lblmode = btnswitch.cget("text").rstrip()
+    if lblmode == "Sim":
+        btnswitch.config(text="Sci")
         btnresult.destroy()
         btnadd.destroy()
         btnsous.destroy()
@@ -78,8 +79,8 @@ def changeMode():
         btnmodulo.destroy()
         ecranScientifique()
 
-    if lblmode == "Scientifique":
-        _mode.set("Simple")
+    if lblmode == "Sci":
+        btnswitch.config(text="Sim")
         btnBaseHex.destroy()
         btnBaseBin.destroy()
         btnBinHex.destroy()
@@ -90,80 +91,78 @@ def changeMode():
         lblhex.destroy()
         ecran()
 
-def conversion(_conversion):
-    global t, n
-    z.set("0")
-    if _conversion == "baseToHex":
-        z.set(hex(t).split("x")[-1])
-    elif _conversion == "baseToBin":
-        z.set(bin(t).split("b")[-1])
-    elif _conversion == "binToHex":
-        try:
-            z.set((hex(int(str(t), 2))).split("x")[-1])
-        except : 
-            z.set("Non binaire")
-    elif _conversion == "binToBase":
-        try:
-            z.set(int(str(t), 2))
-        except : 
-            z.set("Non binaire")
-    elif _conversion == "hexToBin":
-        try:
-            z.set(bin(int(txthex.get(), 16)).split("b")[-1])
-        except : 
-            z.set("Non hexadecimal")
-    elif _conversion == "hexToBase":
-        try:
-            z.set(int(txthex.get(), 16))
-        except : 
-            z.set("Non hexadecimal")
-
-    t, n = 0, 1
-    u.set("0")
+#def conversion(_conversion):
+#    global t, n
+#    z.set("0")
+#    if _conversion == "baseToHex":
+#        z.set(hex(t).split("x")[-1])
+#    elif _conversion == "baseToBin":
+#        z.set(bin(t).split("b")[-1])
+#    elif _conversion == "binToHex":
+#        try:
+#            z.set((hex(int(str(t), 2))).split("x")[-1])
+#        except : 
+#            z.set("Non binaire")
+#    elif _conversion == "binToBase":
+#        try:
+#            z.set(int(str(t), 2))
+#        except : 
+#            z.set("Non binaire")
+#    elif _conversion == "hexToBin":
+#        try:
+#            z.set(bin(int(txthex.get(), 16)).split("b")[-1])
+#        except : 
+#            z.set("Non hexadecimal")
+#    elif _conversion == "hexToBase":
+#        try:
+#            z.set(int(txthex.get(), 16))
+#        except : 
+#            z.set("Non hexadecimal")
+#
+#    t, n = 0, 1
+#    _label_resultat.set("")
 
 
 def ecran__init__():
-    global z, u, _mode
-    btn1 = Button(root, text = "1", command = lambda : x(1))
+    global _label_calcul, label_calcul, _label_resultat, label_resultat, btnswitch, effacer_resultat
+    btn1 = Button(root, text = "1", command = lambda : f_x(1))
     btn1.grid(column = 0, row = 0)
-    btn2 = Button(root, text = "2", command = lambda : x(2))
+    btn2 = Button(root, text = "2", command = lambda : f_x(2))
     btn2.grid(column = 1, row = 0)
-    btn3 = Button(root, text = "3", command = lambda : x(3))
+    btn3 = Button(root, text = "3", command = lambda : f_x(3))
     btn3.grid(column = 2, row = 0)
-    btn4 = Button(root, text = "4", command = lambda : x(4))
+    btn4 = Button(root, text = "4", command = lambda : f_x(4))
     btn4.grid(column = 0, row = 1)
-    btn5 = Button(root, text = "5", command = lambda : x(5))
+    btn5 = Button(root, text = "5", command = lambda : f_x(5))
     btn5.grid(column = 1, row = 1)
-    btn6 = Button(root, text = "6", command = lambda : x(6))
+    btn6 = Button(root, text = "6", command = lambda : f_x(6))
     btn6.grid(column = 2, row = 1)
-    btn7 = Button(root, text = "7", command = lambda : x(7))
+    btn7 = Button(root, text = "7", command = lambda : f_x(7))
     btn7.grid(column = 0, row = 2)
-    btn8 = Button(root, text = "8", command = lambda : x(8))
+    btn8 = Button(root, text = "8", command = lambda : f_x(8))
     btn8.grid(column = 1, row = 2)
-    btn9 = Button(root, text = "9", command = lambda : x(9))
+    btn9 = Button(root, text = "9", command = lambda : f_x(9))
     btn9.grid(column = 2, row = 2)
-    btnclear = Button(root, text = "C", command = lambda : [clear(u), clear(z)])
+    btnclear = Button(root, text = "C", command = lambda : [_label_resultat.set(""), _label_calcul.set("")])
     btnclear.grid(column = 0, row = 3)
     btn0 = Button(root, text = "0", command = lambda : x(0))
     btn0.grid(column = 1, row = 3)
-    btnswitch = Button(root, text = "Mode", command = lambda : changeMode())
+    btnswitch = Button(root, text = "Sim", command = lambda : changeMode())
     btnswitch.grid(column = 4, row = 0, stick = "W")
 
-    u = StringVar()
-    entree2 = Entry(root, textvariable = u)
-    entree2.grid(column = 4, row = 2, ipadx = 40)
-    u.set("0")
-    z = StringVar()
-    entree = Entry(root, textvariable = z)
-    entree.grid(column = 4, row = 3, ipadx = 40)
-    z.set("0")
-    _mode = StringVar()
-    entree3 = Entry(root, textvariable = _mode)
-    entree3.grid(column = 4, row = 0, stick = "E")
-    _mode.set("Simple")
+    _label_resultat = StringVar()
+    label_resultat = Tk.Label(textvariable=_label_resultat)
+    label_resultat.grid(column = 4, row = 3, ipadx = 40)
+    _label_resultat.set("")
+
+    _label_calcul = StringVar()
+    label_calcul = Tk.Label(textvariable=_label_calcul)
+    label_calcul.grid(column = 4, row = 2, ipadx = 40)
+    _label_calcul.set("")
 
     btnquit = Button(root, text = 'Quitter', command = root.destroy)
     btnquit.grid(column = 4, row = 1, stick = "E")
+    effacer_resultat = 0
 
 def ecran():
     global btnresult, btnadd, btnsous, btnmult, btndiv, btnmodulo
